e9c409e49156bbd27aa472877369fb9e7f920abe
Wes Packham <wesleypackham@gmail.com>
2026-03-16 23:35:16 -0400

    Refactored to work with CI/CD github actions

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 SUPABASE_URL = os.environ["SUPABASE_URL"]
 SUPABASE_KEY = os.environ["SUPABASE_KEY"]
 
-SUPABASE_TABLE = "parking data"
+SUPABASE_TABLE = "parking_data"
 URL = "https://parkingavailability.charlotte.edu/decks/stream"
